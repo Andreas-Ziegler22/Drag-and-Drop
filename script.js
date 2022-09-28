df8a264a6cbf1a0f939a165b623ccf3c427ed168
MrBurp <andreass.ziegler@gmail.com>
2022-09-28 13:04:51 +0200

    functions  drag# working

--- script.js
+++ script.js
@@ -3,3 +3,29 @@ const empties = document.querySelectorAll(".empty");
 
 fill.addEventListener("dragstart", dragStart);
 fill.addEventListener("dragend", dragEnd);
+
+for (empty of empties) {
+  empty.addEventListener("dragover", dragOver);
+  empty.addEventListener("dragenter", dragEnter);
+  empty.addEventListener("dragleave", dragLeave);
+  empty.addEventListener("drop", dragDrop);
+}
+
+function dragStart() {
+  console.log("drag start");
+}
+function dragEnd() {
+  console.log("drag end");
+}
+function dragOver() {
+  console.log("drag Over");
+}
+function dragEnter() {
+  console.log("drag enter");
+}
+function dragLeave() {
+  console.log("drag leave");
+}
+function dragDrop() {
+  console.log("drop");
+}
